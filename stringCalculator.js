7af6939f3e07e07f15bb695e251b4dd190ef2d69
Tejas chaudhari <tejaschaudhari038@gmail.com>
2024-09-03 17:44:42 +0530

    test cases and code formatting

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,32 +1,33 @@
 class StringCalculator {
-    constructor() {
-      this.callCount = 0;
-    }
-
-    add(numbers) {
-      this.callCount++;
-      if (numbers === "") return 0;
-
-      let delimiter = /[\n,]/; // Default delimiters
-      if (numbers.startsWith("//")) {
-        const parts = numbers.split('\n');
-        delimiter = new RegExp(parts[0].substring(2), 'g');
-        numbers = parts[1];
-      }
-
-      const numbersArray = numbers.split(delimiter).map(Number);
-      const negatives = numbersArray.filter(num => num < 0);
-      if (negatives.length > 0) {
-        throw new Error(`negative numbers not allowed ${negatives.join(',')}`);
-      }
-
-      return numbersArray.filter(num => num <= 1000).reduce((sum, num) => sum + num, 0);
-    }
-
-    getCalledCount() {
-      return this.callCount;
-    }
+  constructor() {
+    this.callCount = 0;
   }
-  
-  module.exports = StringCalculator;
-  
+
+  add(numbers) {
+    this.callCount++;
+    if (numbers === "") return 0;
+
+    let delimiter = /[\n,]/; // Default delimiters
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(parts[0].substring(2), "g");
+      numbers = parts[1];
+    }
+
+    const numbersArray = numbers.split(delimiter).map(Number);
+    const negatives = numbersArray.filter((num) => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
+    }
+
+    return numbersArray
+      .filter((num) => num <= 1000)
+      .reduce((sum, num) => sum + num, 0);
+  }
+
+  getCalledCount() {
+    return this.callCount;
+  }
+}
+
+module.exports = StringCalculator;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,44 +1,45 @@
-const StringCalculator = require('./stringCalculator');
+const StringCalculator = require("./stringCalculator");
 
-test('Empty string returns 0', () => {
+test("Empty string returns 0", () => {
   const calc = new StringCalculator();
   expect(calc.add("")).toBe(0);
 });
 
-test('Single number returns that number', () => {
-    const calc = new StringCalculator();
-    expect(calc.add("1")).toBe(1);
-  });
+test("Single number returns that number", () => {
+  const calc = new StringCalculator();
+  expect(calc.add("1")).toBe(1);
+});
 
-  test('Two numbers separated by comma return their sum', () => {
-    const calc = new StringCalculator();
-    expect(calc.add("1,5")).toBe(6);
-  });
+test("Two numbers separated by comma return their sum", () => {
+  const calc = new StringCalculator();
+  expect(calc.add("1,5")).toBe(6);
+});
 
+test("Handles new lines between numbers", () => {
+  const calc = new StringCalculator();
+  expect(calc.add("1\n2,3")).toBe(6);
+});
 
-  test('Handles new lines between numbers', () => {
-    const calc = new StringCalculator();
-    expect(calc.add("1\n2,3")).toBe(6);
-  });
+test("Handles custom delimiters", () => {
+  const calc = new StringCalculator();
+  expect(calc.add("//;\n1;2")).toBe(3);
+});
 
-  test('Handles custom delimiters', () => {
-    const calc = new StringCalculator();
-    expect(calc.add("//;\n1;2")).toBe(3);
-  });
+test("Throws error for negative numbers", () => {
+  const calc = new StringCalculator();
+  expect(() => calc.add("1,-2")).toThrow("negative numbers not allowed -2");
+});
 
-  test('Throws error for negative numbers', () => {
-    const calc = new StringCalculator();
-    expect(() => calc.add("1,-2")).toThrow("negative numbers not allowed -2");
-  });
+test("Throws error for multiple negative numbers", () => {
+  const calc = new StringCalculator();
+  expect(() => calc.add("1,-2,-3")).toThrow(
+    "negative numbers not allowed -2,-3"
+  );
+});
 
-  test('Throws error for multiple negative numbers', () => {
-    const calc = new StringCalculator();
-    expect(() => calc.add("1,-2,-3")).toThrow("negative numbers not allowed -2,-3");
-  });
-
-  test('Count the number of times Add() is called', () => {
-    const calc = new StringCalculator();
-    calc.add("1,2");
-    calc.add("3");
-    expect(calc.getCalledCount()).toBe(2);
-  });
+test("Count the number of times Add() is called", () => {
+  const calc = new StringCalculator();
+  calc.add("1,2");
+  calc.add("3");
+  expect(calc.getCalledCount()).toBe(2);
+});
